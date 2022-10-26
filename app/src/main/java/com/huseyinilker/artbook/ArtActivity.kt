31d7bf753eeeec0598a99418ee1b0ca825cc596b
hgulese <hgulese02@gmail.com>
2022-10-26 22:30:59 +0300

    Add Option Menu

--- app/src/main/java/com/huseyinilker/artbook/ArtActivity.kt
+++ app/src/main/java/com/huseyinilker/artbook/ArtActivity.kt
@@ -1,8 +1,15 @@
 package com.huseyinilker.artbook
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.huseyinilker.artbook.databinding.ActivityArtBinding
 
 class ArtActivity : AppCompatActivity() {
@@ -23,6 +30,23 @@ class ArtActivity : AppCompatActivity() {
     }
 
     fun selectImage(view: View) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // rationale
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", View.OnClickListener {
+                    // request permission
+                }).show()
+
+            } else {
+                // request permission
+            }
+
+        } else {
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        }
 
     }
 
--- app/src/main/java/com/huseyinilker/artbook/MainActivity.kt
+++ app/src/main/java/com/huseyinilker/artbook/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.huseyinilker.artbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.huseyinilker.artbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +19,23 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.art_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val intent = Intent(this@MainActivity, ArtActivity::class.java)
+        return when (item.itemId) {
+            R.id.addArt -> {
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
